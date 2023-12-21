e909173b565a47593f41f7237bcb396b1e9560d0
Márcio Gabriel <37910437+KozielGPC@users.noreply.github.com>
2023-12-20 22:49:03 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -169,7 +169,7 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl enable docker
 
-# Kubernetes
+# Minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
