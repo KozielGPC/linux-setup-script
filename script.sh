40bf44056a6ba78ddc6814d9da32003477ba1562
Márcio Gabriel <37910437+KozielGPC@users.noreply.github.com>
2023-12-20 22:42:51 -0300

    Add script.sh

---
+++ script.sh
@@ -0,0 +1,177 @@
+#!/usr/bin/bash
+
+# Commons
+sudo apt update && sudo apt upgrade -y
+sudo apt install libfuse2 software-properties-common gnupg apt-transport-https ca-certificates apt-transport-https wget curl htop -y
+
+
+# Another Redis Desktop Manager
+wget https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v1.6.1/Another-Redis-Desktop-Manager.1.6.1.AppImage
+chmod +x Another-Redis-Desktop-Manager.1.6.1.AppImage
+./Another-Redis-Desktop-Manager.1.6.1.AppImage &
+
+
+# MongoDB
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc |  sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse
+sudo apt update
+sudo apt install mongodb-org -y
+
+
+# MongoDB Compass
+sudo apt update
+wget https://downloads.mongodb.com/compass/mongodb-compass_1.39.0_amd64.deb
+sudo apt install ./mongodb-compass_1.39.0_amd64.deb
+
+
+# Dbeaver
+sudo apt update && sudo apt -y install default-jdk
+curl -fsSL https://dbeaver.io/debs/dbeaver.gpg.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/dbeaver.gpg
+echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+sudo apt update && sudo apt install dbeaver-ce
+
+
+# Insomnia
+sudo apt update && sudo apt upgrade
+echo "deb [trusted=yes arch=amd64] https://download.konghq.com/insomnia-ubuntu/ default all" | sudo tee /etc/apt/sources.list.d/insomnia.list
+sudo apt update
+sudo apt install insomnia
+
+
+# VSCode
+sudo apt update && sudo apt upgrade -y
+
+wget -O- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/vscode.gpg
+
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.microsoft.com/repos/vscode stable main | sudo tee /etc/apt/sources.list.d/vscode.list
+
+sudo apt update
+
+sudo apt install code
+
+# VSCode extensions
+code --install-extension eamodio.gitlens --force
+code --install-extension dbaeumer.vscode-eslint --force
+code --install-extension esbenp.prettier-vscode --force
+code --install-extension GitHub.github-vscode-theme --force
+code --install extension codeium.codeium
+code --install extension firsttris.vscode-jest-runner
+
+
+# Git
+sudo apt install git
+
+# ZSH
+sudo apt install zsh
+
+# OhMyZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# NVM and node 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+
+# Enable auto switch node version
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+echo "# place this after nvm initialization!
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+auto-switch-node-version() {
+  NVMRC_PATH=$(nvm_find_nvmrc)
+  CURRENT_NODE_VERSION=$(nvm version)
+
+  if [[ ! -z "$NVMRC_PATH" ]]; then
+    # .nvmrc file found!
+
+    # Read the file
+    REQUESTED_NODE_VERSION=$(cat $NVMRC_PATH)
+
+    # Find an installed Node version that satisfies the .nvmrc
+    MATCHED_NODE_VERSION=$(nvm_match_version $REQUESTED_NODE_VERSION)
+
+    if [[ ! -z "$MATCHED_NODE_VERSION" && $MATCHED_NODE_VERSION != "N/A" ]]; then
+      # A suitable version is already installed.
+
+      # Clear any warning suppression
+      unset AUTOSWITCH_NODE_SUPPRESS_WARNING
+
+      # Switch to the matched version ONLY if necessary
+      if [[ $CURRENT_NODE_VERSION != $MATCHED_NODE_VERSION ]]; then
+        nvm use $REQUESTED_NODE_VERSION
+      fi
+    else
+      # No installed Node version satisfies the .nvmrc.
+
+      # Quit silently if we already just warned about this exact .nvmrc file, so you
+      # only get spammed once while navigating around within a single project.
+      if [[ $AUTOSWITCH_NODE_SUPPRESS_WARNING == $NVMRC_PATH ]]; then
+        return
+      fi
+
+      # Convert the .nvmrc path to a relative one (if possible) for readability
+      RELATIVE_NVMRC_PATH="$(realpath --relative-to=$(pwd) $NVMRC_PATH 2> /dev/null || echo $NVMRC_PATH)"
+
+      # Print a clear warning message
+      echo ""
+      echo "WARNING"
+      echo "  Found file: $RELATIVE_NVMRC_PATH"
+      echo "  specifying: $REQUESTED_NODE_VERSION"
+      echo "  ...but no installed Node version satisfies this."
+      echo "  "
+      echo "  Current node version: $CURRENT_NODE_VERSION"
+      echo "  "
+      echo "  You might want to run \"nvm install\""
+
+      # Record that we already warned about this unsatisfiable .nvmrc file
+      export AUTOSWITCH_NODE_SUPPRESS_WARNING=$NVMRC_PATH
+    fi
+  else
+    # No .nvmrc file found.
+
+    # Clear any warning suppression
+    unset AUTOSWITCH_NODE_SUPPRESS_WARNING
+
+    # Revert to default version, unless that's already the current version.
+    if [[ $CURRENT_NODE_VERSION != $(nvm version default)  ]]; then
+      nvm use default
+    fi
+  fi
+}
+
+# Run the above function in ZSH whenever you change directory
+autoload -U add-zsh-hook
+add-zsh-hook chpwd auto-switch-node-version
+auto-switch-node-version" >> $HOME/.zshrc
+
+
+nvm install 18.15
+nvm use 18.15
+
+# Yarn
+npm install --global yarn
+
+
+
+# Docker
+sudo apt update
+sudo apt install docker.io -y
+sudo usermod -aG docker $USER
+sudo chmod 666 /var/run/docker.sock
+
+# Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo systemctl enable docker
+
+# Kubernetes
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/
